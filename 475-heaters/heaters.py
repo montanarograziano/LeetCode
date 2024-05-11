3be8a053739e4ae7980da8c284a689ec66dcba1b
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2024-05-11 17:05:16 +0200

    Time: 454 ms (14.28%) | Memory: 20.1 MB (72.12%) - LeetSync

---
+++ 475-heaters/heaters.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        houses.sort()
+        heaters.sort()
+        
+        def can_cover_all(radius):
+            i, j = 0, 0
+            while i < len(houses) and j < len(heaters):
+                if abs(houses[i] - heaters[j]) <= radius:
+                    i += 1
+                else:
+                    j += 1
+            return i == len(houses)
+        
+        left, right = 0, max(houses[-1], heaters[-1])
+        
+        while left < right:
+            mid = (right + left) // 2
+            if can_cover_all(mid):
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
