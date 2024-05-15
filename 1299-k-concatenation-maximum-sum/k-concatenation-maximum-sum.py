77dacb30f86d7349516a6b3b88ae70e49ff747f8
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2024-05-15 22:14:44 +0200

    Time: 363 ms (44.32%) | Memory: 31 MB (43.75%) - LeetSync

---
+++ 1299-k-concatenation-maximum-sum/k-concatenation-maximum-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def kConcatenationMaxSum(self, nums: List[int], k: int) -> int:
+        MOD = 10 ** 9 + 7
+        
+        def kadane(k):
+            res = cur = 0
+            for _ in range(k):
+                for num in nums:
+                    cur = max(cur + num, num)
+                    res = max(res, cur)
+            
+            return res
+        
+        return (max(0, k - 2) * max(0, sum(nums)) + kadane(min(2, k))) % MOD
