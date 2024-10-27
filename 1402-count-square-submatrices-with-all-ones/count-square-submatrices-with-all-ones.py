a469fda6240a9b74b3912628599d3b9a67cfa778
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2024-10-27 13:15:32 +0100

    Time: 391 ms (7.25%) | Memory: 37.4 MB (5.14%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.py
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.py
@@ -2,22 +2,23 @@ class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
         ROWS, COLS = len(matrix), len(matrix[0])
         cache = {}
+        res = 0
 
         def helper(r, c):
-            if r not in range(ROWS) or c not in range(COLS):
+            if r not in range(ROWS) or c not in range(COLS) or not matrix[r][c]:
                 return 0
             
             if (r, c) not in cache:
-                cache[(r, c)] = 0
                 down = helper(r + 1, c)
                 right = helper(r, c + 1)
                 diag = helper(r + 1, c + 1)
-                if matrix[r][c] == 1:
-                    cache[(r, c)] = min(down, right, diag) + 1
+                cache[(r, c)] = min(down, right, diag) + 1
 
             return cache[(r, c)]
         
-        helper(0, 0)
+        for r in range(ROWS):
+            for c in range(COLS):
+                res += helper(r, c)
         return sum(cache.values())
             
 
