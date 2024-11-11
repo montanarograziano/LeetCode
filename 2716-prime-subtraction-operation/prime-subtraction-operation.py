a38c8ae87ac70e6159ca8d97f70a419d796051e3
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2024-11-11 14:34:56 +0100

    Time: 108 ms (25.14%) | Memory: 16.9 MB (35.50%) - LeetSync

---
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.py
@@ -0,0 +1,24 @@
+class Solution:
+    def primeSubOperation(self, nums: List[int]) -> bool:
+        primes = [0, 0]
+        def is_prime(n):
+            for i in range(2, int(sqrt(n)) + 1):
+                if n % i == 0:
+                    return False
+            return True
+        
+        for i in range(2, max(nums)):
+            if is_prime(i):
+                primes.append(i)
+            else:
+                primes.append(primes[i - 1])
+        prev = 0
+        for n in nums:
+            upper_bound = n - prev
+            largest_p = primes[upper_bound - 1]
+            if n - largest_p <= prev:
+                return False
+            
+            prev = n - largest_p
+        
+        return True
