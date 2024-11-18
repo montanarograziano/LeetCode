87316b6094ddc1742f5dae48c9c0994173ea0cf9
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2024-11-18 22:31:39 +0100

    Time: 2 ms (59.15%) | Memory: 16.9 MB (5.79%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.py
@@ -0,0 +1,21 @@
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        if k == 0:
+            return [0] * len(code)
+
+        res = []
+        n = len(code)
+        if k > 0:
+            for i, _ in enumerate(code):
+                cur = 0
+                for j in range(i + 1, i + k + 1):
+                    cur += code[j % n]
+                res.append(cur)
+        else:
+            for i, _ in enumerate(code):
+                cur = 0
+                for j in range(i + k, i):
+                    cur += code[j % n]
+                res.append(cur)
+
+        return res
