b10d31840bc46137873dbeecbc3768937c8728f9
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2024-11-23 14:25:38 +0100

    Time: 1824 ms (97.47%) | Memory: 29.4 MB (6.38%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.py
+++ 1972-rotating-the-box/rotating-the-box.py
@@ -1,28 +1,14 @@
 class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
         N, M = len(box), len(box[0])
-        res = [["."] * M for _ in range(N)]
-        for i in range(N):
-            for j in range(M):
-                if box[i][j] == '*':
-                    res[i][j] = '*'
-
-        for r, row in enumerate(box):
-            stones = row.count("#")
-            obst_idx = row.index('*') if '*' in row else M
+        for r in range(N):
             i = M - 1
-            while stones and i >= 0:
-                if box[r][i] == ".":
-                    j = i - 1
-                    while j >= 0:
-                        if box[r][j] == '#':
-                            box[r][i] = '#'
-                            box[r][j] = '.'
-                            break
-                        elif box[r][j] == '*':
-                            break
-                        else:
-                            j -= 1
-                i -= 1
+            for c in reversed(range(M)):
+                if box[r][c] == '#':
+                    box[r][i], box[r][c] = box[r][c], box[r][i]
+                    i -= 1
+                elif box[r][c] == '*':
+                    i = c - 1
+            
         return list(zip(*box[::-1]))
                     
