bcd87ac8f281dd0265d0385528912ea5cda5f941
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2025-01-13 18:01:25 +0100

    Time: 55 ms (98.85%) | Memory: 21.9 MB (6.03%) - LeetSync

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canBeValid(self, s: str, locked: str) -> bool:
+        length = len(s)
+        if length % 2:
+            return False
+
+        open_brackets = []
+        unlocked = []
+
+        for i in range(length):
+            if locked[i] == "0":
+                unlocked.append(i)
+            elif s[i] == "(":
+                open_brackets.append(i)
+            elif s[i] == ")":
+                if open_brackets:
+                    open_brackets.pop()
+                elif unlocked:
+                    unlocked.pop()
+                else:
+                    return False
+
+        # Match remaining open brackets and the unlocked characters
+        while open_brackets and unlocked and open_brackets[-1] < unlocked[-1]:
+            open_brackets.pop()
+            unlocked.pop()
+
+        if open_brackets:
+            return False
+
+        return True
