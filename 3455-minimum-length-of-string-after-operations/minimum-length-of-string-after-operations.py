1e9875837d8fb9764a2a0ed37f906ccb048df47a
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2025-01-13 18:13:05 +0100

    Time: 134 ms (66.35%) | Memory: 19 MB (5.71%) - LeetSync

---
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        c = Counter(s)
+        delete_cnt = 0
+        for k,v in c.items():
+                if v % 2:
+                    delete_cnt += v - 1
+                else:
+                    delete_cnt += v - 2
+        return len(s) - delete_cnt
+
+
+
+        
