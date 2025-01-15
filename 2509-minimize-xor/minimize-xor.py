e5ee34451a7a88452ef363f9342a9a5ce9233dcc
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2025-01-15 11:41:03 +0100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (41.60%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        a, b = num1.bit_count(), num2.bit_count()
+        if a >= b:
+            for _ in range(a - b):
+                num1 = num1 & (num1 - 1)
+        else:
+            for _ in range(b - a):
+                num1 = ~(~num1 & (~num1 - 1))
+        return num1
+        
