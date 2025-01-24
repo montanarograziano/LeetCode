f78e6161a27cca05404c1aa6d01175368909f21b
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2025-01-24 14:43:53 +0100

    Time: 11 ms (89.61%) | Memory: 19.5 MB (62.37%) - LeetSync

---
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        tot_rows = [0] * ROWS
+        tot_cols = [0] * COLS
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j]:
+                    tot_rows[i] += 1
+                    tot_cols[j] += 1  
+        res = 0
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] and max(tot_rows[i], tot_cols[j]) > 1:
+                    res += 1
+        
+        return res
