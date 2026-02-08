093ea0a8f2d2b4b28d8a3596397055ef01252ba4
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2026-02-08 14:26:54 +0100

    Create 2164-TwoBestNonOverlappingEvents

---
+++ 2164-TwoBestNonOverlappingEvents/2164-TwoBestNonOverlappingEvents.py
@@ -0,0 +1,25 @@
+# Last updated: 08/02/2026, 14:26:53
+class Solution:
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        events.sort(key=lambda x: x[0])
+        n = len(events)
+        suffix = [0] * n
+        suffix[n - 1] = events[n - 1][2]
+        for i in range(n - 2, -1, -1):
+            suffix[i] = max(events[i][2], suffix[i + 1])
+        
+        res = 0
+        for i in range(n):
+            left, right = i + 1, n - 1
+            next_idx = -1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if events[mid][0] > events[i][1]:
+                    next_idx = mid
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            if next_idx != -1:
+                res = max(res, events[i][2] + suffix[next_idx]) 
+        res = max(max((x[2] for x in events)), res)  
+        return res
