a7486bccaf601c12c23d6a649170ae89d853bade
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2026-02-08 14:24:59 +0100

    Create 3600-FindTheKThCharacterInStringGameI

---
+++ 3600-FindTheKThCharacterInStringGameI/3600-FindTheKThCharacterInStringGameI.py
@@ -0,0 +1,20 @@
+# Last updated: 08/02/2026, 14:24:59
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        l = 1
+        s = "a"
+
+        def next_s(s):
+            out = ""
+            for c in s:
+                out += "a" if c == "z" else chr(ord(c) + 1)
+            return out
+
+        while l < k:
+            cur = next_s(s)
+            s += cur
+            l += len(cur)
+        
+        return s[k - 1]
+        
+        
