149836b7ffcb36553580d54dc24879a52f5297cd
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2026-02-08 14:27:42 +0100

    Create 1558-CourseScheduleIv

---
+++ 1558-CourseScheduleIv/1558-CourseScheduleIv.py
@@ -0,0 +1,26 @@
+# Last updated: 08/02/2026, 14:27:42
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        adj = defaultdict(list)
+        for pre, crs  in prerequisites:
+            adj[crs].append(pre)
+        
+        def dfs(crs):
+            if crs not in prereqMap:
+                prereqMap[crs] = set()
+                for prereq in adj[crs]:
+                    prereqMap[crs] |= dfs(prereq)
+                prereqMap[crs].add(crs)
+            
+            return prereqMap[crs]
+
+
+        prereqMap = {}
+        for crs in range(numCourses):
+            dfs(crs)
+        
+        res = []
+        for u,v in queries:
+            res.append(u in prereqMap[v])
+        
+        return res
