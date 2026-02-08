5869349a993228d3e5843306e0fd88e444cb6807
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2026-02-08 14:28:22 +0100

    Create 1137-HeightChecker

---
+++ 1137-HeightChecker/1137-HeightChecker.py
@@ -0,0 +1,10 @@
+# Last updated: 08/02/2026, 14:28:22
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+        res = 0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                res += 1
+        
+        return res
