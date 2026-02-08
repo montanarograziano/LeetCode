7631e4ad42b6a45ae511fef0eb7386dd68d3e96a
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2026-02-08 14:25:21 +0100

    Create 2794-MaximumNumberOfMovesInAGrid

---
+++ 2794-MaximumNumberOfMovesInAGrid/2794-MaximumNumberOfMovesInAGrid.py
@@ -0,0 +1,18 @@
+# Last updated: 08/02/2026, 14:25:21
+class Solution:
+    def maxMoves(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        cache = {}
+        def dp(r, c):
+            if (r,c) in cache:
+                return cache[(r, c)]
+            
+            res = 0
+            for dr, dc in [(r - 1, c + 1), (r, c + 1), (r + 1, c + 1)]:
+                if dr in range(ROWS) and dc in range(COLS) and grid[r][c] < grid[dr][dc]:
+                    res = max(res, 1 + dp(dr, dc)) 
+            cache[(r, c)] = res
+            return res
+        
+        return max(dp(row, 0) for row in range(ROWS))
+        
