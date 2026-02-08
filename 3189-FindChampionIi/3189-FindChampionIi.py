0233b3a9ddf97349a99e59713ddc4b3086fbbcd2
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2026-02-08 14:25:13 +0100

    Create 3189-FindChampionIi

---
+++ 3189-FindChampionIi/3189-FindChampionIi.py
@@ -0,0 +1,12 @@
+# Last updated: 08/02/2026, 14:25:12
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        if n == 1:
+            return 0
+        res = -1
+        visit = set([i for i in range(n)])
+        for l, r in edges:
+            if r in visit:
+                visit.remove(r)
+        
+        return res if len(visit) > 1 else visit.pop()
