7d53f26669b1b428b2850c920f31e681ecab066e
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2026-02-08 14:27:08 +0100

    Create 1972-RotatingTheBox

---
+++ 1972-RotatingTheBox/1972-RotatingTheBox.py
@@ -0,0 +1,15 @@
+# Last updated: 08/02/2026, 14:27:08
+class Solution:
+    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+        N, M = len(box), len(box[0])
+        for r in range(N):
+            i = M - 1
+            for c in reversed(range(M)):
+                if box[r][c] == '#':
+                    box[r][i], box[r][c] = box[r][c], box[r][i]
+                    i -= 1
+                elif box[r][c] == '*':
+                    i = c - 1
+            
+        return list(zip(*box[::-1]))
+                    
