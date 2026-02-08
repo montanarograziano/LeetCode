84531107aad675f062327718d4a6243408215d19
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2026-02-08 14:26:58 +0100

    Create 2132-Convert1dArrayInto2dArray

---
+++ 2132-Convert1dArrayInto2dArray/2132-Convert1dArrayInto2dArray.py
@@ -0,0 +1,13 @@
+# Last updated: 08/02/2026, 14:26:58
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        if m * n != len(original):
+            return []
+        
+        res = [[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                idx = i * n + j
+                res[i][j] = original[idx]
+        
+        return res
