75970ebdb8abed5beb2c557e19350486fec2a7f4
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2026-02-08 14:27:01 +0100

    Create 2076-SumOfDigitsOfStringAfterConvert

---
+++ 2076-SumOfDigitsOfStringAfterConvert/2076-SumOfDigitsOfStringAfterConvert.py
@@ -0,0 +1,7 @@
+# Last updated: 08/02/2026, 14:27:01
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        s = "".join(str(ord(ch) - 96) for ch in s)
+        for _ in range(k):
+            s = str(sum(int(ch) for ch in s))
+        return int(s)
