5319bb380f75533aca6f52917b8ac6b0be6a1f56
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2026-02-08 14:26:03 +0100

    Create 2384-RootEqualsSumOfChildren

---
+++ 2384-RootEqualsSumOfChildren/2384-RootEqualsSumOfChildren.py
@@ -0,0 +1,10 @@
+# Last updated: 08/02/2026, 14:26:02
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        return root.val == root.left.val + root.right.val
