f29047a648701ea0da5de27761b522bfb0287475
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2026-02-08 14:27:59 +0100

    Create 1402-CountSquareSubmatricesWithAllOnes

---
+++ 1402-CountSquareSubmatricesWithAllOnes/1402-CountSquareSubmatricesWithAllOnes.py
@@ -0,0 +1,26 @@
+# Last updated: 08/02/2026, 14:27:59
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        cache = {}
+        res = 0
+
+        def helper(r, c):
+            if r not in range(ROWS) or c not in range(COLS) or not matrix[r][c]:
+                return 0
+            
+            if (r, c) not in cache:
+                down = helper(r + 1, c)
+                right = helper(r, c + 1)
+                diag = helper(r + 1, c + 1)
+                cache[(r, c)] = min(down, right, diag) + 1
+
+            return cache[(r, c)]
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                res += helper(r, c)
+        return sum(cache.values())
+            
+
+        
